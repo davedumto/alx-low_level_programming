13d51cef9d57390a17b6772730f1475419b6e635
davedumto <ejeredavid2001@gmail.com>
2022-10-27 04:40:53 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,54 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 
 /**
- * main - Generates random valid passwords for the
- *        program 101-crackme.
- *
- * Return: Always 0.
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
  */
-int main(void)
+void print_number(int n)
 {
-	char password[84];
-	int index = 0, sum = 0, diff_half1, diff_half2;
+	unsigned int m, d, count;
 
-	srand(time(0));
-
-	while (sum < 2772)
+	if (n < 0)
 	{
-		password[index] = 33 + rand() % 94;
-		sum += password[index++];
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
 	}
 
-	password[index] = '\0';
+	d = m;
+	count = 1;
 
-	if (sum != 2772)
+	while (d > 9)
 	{
-		diff_half1 = (sum - 2772) / 2;
-		diff_half2 = (sum - 2772) / 2;
-		if ((sum - 2772) % 2 != 0)
-			diff_half1++;
-
-		for (index = 0; password[index]; index++)
-		{
-			if (password[index] >= (33 + diff_half1))
-			{
-				password[index] -= diff_half1;
-				break;
-			}
-		}
-		for (index = 0; password[index]; index++)
-		{
-			if (password[index] >= (33 + diff_half2))
-			{
-				password[index] -= diff_half2;
-				break;
-			}
-		}
+		d /= 10;
+		count *= 10;
 	}
 
-	printf("%s", password);
-
-	return (0);
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
